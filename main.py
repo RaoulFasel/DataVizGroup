aea849f8f7a98deb27a51752942ff3d0ab9488d0
Raoul Fasel <raoul@fasel.nl>
2015-06-07 21:54:59 +0200

    added image url and fixed errors in parsing(free games etc)

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ results = []
 
 def get_request(url):
     http = urllib3.PoolManager()
-    r =  http.request('GET', url)
-    return json.loads(r.data.decode('utf8'))
+    r = http.request('GET', url)
+    return json.loads(r.data.decode('ISO-8859-1'))
 
-header = "name, price, genres, metacritic, release, likes, achievements, linux, mac, windows, owners, players_forever, players_2weeks, average_forever, average_2weeks,median_forever,median_2weeks"
-print(header)
+header = "name, price, genres, metacritic, release, likes, achievements, linux, mac, windows, image, owners, players_forever, players_2weeks, average_forever, average_2weeks,median_forever,median_2weeks"
 
 top = get_request("http://steamspy.com/api.php?request=top100forever")
 
@@ -24,20 +23,45 @@ for x in top:
     try:
 
         data.append(bb[x]['data']["name"])
-        data.append(bb[x]['data']["price_overview"]["initial"])
+        if bb[x]['data']['is_free']:
+            data.append("free")
+        else:
+            data.append(bb[x]['data']["price_overview"]["initial"])
         genres = []
         for y in bb[x]['data']["genres"]:
             genres.append(y["description"])
-        print(genres)
         data.append(genres)
-
-        data.append(bb[x]['data']["metacritic"]["score"])
-        data.append(bb[x]['data']["release_date"]["date"])
+        if "metacritic" in bb[x]['data']:
+            data.append(bb[x]['data']["metacritic"]["score"])
+        else:
+            data.append("Unkown")
+        if "release_date" in bb[x]['data']:
+            data.append(bb[x]['data']["release_date"]["date"])
+        else:
+            data.append("Unkown")
         data.append(bb[x]['data']["recommendations"]["total"])
-        data.append(bb[x]['data']["achievements"]["total"])
+        if "achievements" in bb[x]['data']:
+            data.append(bb[x]['data']["achievements"]["total"])
+        else:
+            data.append("None")
         data.append(bb[x]['data']["platforms"]["linux"])
         data.append(bb[x]['data']["platforms"]["mac"])
         data.append(bb[x]['data']["platforms"]["windows"])
+        if "header_image" in bb[x]['data']:
+            data.append(bb[x]['data']["header_image"])
+        else:
+            data.append("none")
+
+
+
+
+
+
+    except (KeyError, TypeError):
+        print("error steam")
+        print(x)
+
+    try:
         data.append(top[x]["owners"])
         data.append(top[x]["players_forever"])
         data.append(top[x]["players_2weeks"])
@@ -45,16 +69,12 @@ for x in top:
         data.append(top[x]["average_2weeks"])
         data.append(top[x]["median_forever"])
         data.append(top[x]["median_2weeks"])
-        print(data)
-        results.append(data)
-
-
-
-    except (KeyError,TypeError):
-        pass
-        print("error")
+    except (KeyError, TypeError):
+        print("error steampsy")
+        print(x)
+    print(data)
+    results.append(data)
 print(results)
-print(type(results))
 csvfile.write(header)
 csvfile.write("\n")
 csvwriter.writerows(results)
