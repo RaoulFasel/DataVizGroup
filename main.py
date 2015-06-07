901e2edd36a88d7b52c1fd630f915f1b229b558b
Raoul Fasel <raoul@fasel.nl>
2015-06-07 22:21:10 +0200

    added cvs edit, 4 genre collums

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_request(url):
     r = http.request('GET', url)
     return json.loads(r.data.decode('ISO-8859-1'))
 
-header = "name, price, genres, metacritic, release, likes, achievements, linux, mac, windows, image, owners, players_forever, players_2weeks, average_forever, average_2weeks,median_forever,median_2weeks"
+header = "name, price, genre1, genre2, genre3, genre4, metacritic, release, likes, achievements, linux, mac, windows, image, owners, players_forever, players_2weeks, average_forever, average_2weeks,median_forever,median_2weeks"
 
 top = get_request("http://steamspy.com/api.php?request=top100forever")
 
@@ -27,10 +27,16 @@ for x in top:
             data.append("free")
         else:
             data.append(bb[x]['data']["price_overview"]["initial"])
-        genres = []
-        for y in bb[x]['data']["genres"]:
-            genres.append(y["description"])
-        data.append(genres)
+
+        genre_length = len(bb[x]['data']['genres'])
+        if genre_length>4:
+            genre_length=4
+
+        for y in range(genre_length):
+            data.append(bb[x]['data']['genres'][y]["description"])
+        for y in range(4-genre_length):
+            data.append("")
+
         if "metacritic" in bb[x]['data']:
             data.append(bb[x]['data']["metacritic"]["score"])
         else:
@@ -74,7 +80,31 @@ for x in top:
         print(x)
     print(data)
     results.append(data)
-print(results)
+#print(results)
 csvfile.write(header)
 csvfile.write("\n")
 csvwriter.writerows(results)
+json_data = []
+for x in results:
+    item = {}
+    item["name"] = x[0]
+    item["price"] = x[1]
+    item["genre"] = x[2][0]
+    item["genre2"] = x[0][1]
+    item["genre3"] = x[0][2]
+    item["genre4"] = x[0][3]
+    item["metacritic"] = x[0]
+    item["release"] = x[0]
+    item["likes"] = x[0]
+    item["achievements"] = x[0]
+    item["linux"] = x[0]
+    item["mac"] = x[0]
+    item["windows"] = x[0]
+    item["owners"] = x[0]
+    item["players_forever"] = x[0]
+    item["players_2weeks"] = x[0]
+    item["average_forever"] = x[0]
+    item["average_2weeks"] = x[0]
+    item["median_forever"] = x[0]
+    item["median_2weeks"] = x[0]
+    item["waste"] = x[0]
